5f420802cec5eeb9e75aa526d1984ef70e0be113
jm <jm@d5cb13e6-57fc-0310-9ab5-ddaedd36d86d>
2006-09-04 09:11:09 +0000

    - add new Role class

---
+++ src/java/de/ingrid/portal/security/role/IngridRole.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2006 wemove digital solutions. All rights reserved.
+ */
+package de.ingrid.portal.security.role;
+
+/**
+ * Class for ingrid specific roles. This Class holds ingrid role constants 
+ * at the moment, but should be extended if ingrid specific role functionality
+ * must be implemented.
+ *
+ * @author joachim@wemove.com
+ */
+public class IngridRole {
+
+    public static String ROLE_ADMIN_PORTAL = "admin-portal";
+    public static String ROLE_ADMIN_PARTNER = "admin-partner";
+    public static String ROLE_ADMIN_PROVIDER = "admin-provider";
+
+}
